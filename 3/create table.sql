75c7bd2daa92e1093404b97ea55301ca608da803
crptbosyak <75790510+crptbosyak@users.noreply.github.com>
2021-06-21 16:26:14 +0300

    Update create table.sql

--- 3/create table.sql
+++ 3/create table.sql
@@ -4,7 +4,7 @@ owners_name nvarchar(30)not null,
 create_date nvarchar(30)NOT NULL,
 planned_start_date date not null,
 completion_date date ,
-status nvarchar(30)not null,
+status nvarchar(30)not null default'ACCEPTED',
 car_id int(30) not null,
 master_id int(30)
 );
